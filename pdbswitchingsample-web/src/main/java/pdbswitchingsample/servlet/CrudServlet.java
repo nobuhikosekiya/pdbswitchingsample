9cdbcc89d76f7f0d1eb04d77193d49504f97f094
Nobuhiko Sekiya <nobuhiko.sekiya@jp.oracle.com>
2015-12-08 17:50:02 +0900

    refactor

--- pdbswitchingsample-web/src/main/java/pdbswitchingsample/servlet/CrudServlet.java
+++ pdbswitchingsample-web/src/main/java/pdbswitchingsample/servlet/CrudServlet.java
@@ -1,4 +1,4 @@
-package com.example;
+package pdbswitchingsample.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
--- pdbswitchingsamplecallbck-jar/src/main/java/pdbswitchingsample/callback/PDBSwitchLabelingCallback.java
+++ pdbswitchingsamplecallbck-jar/src/main/java/pdbswitchingsample/callback/PDBSwitchLabelingCallback.java
@@ -1,4 +1,4 @@
-package com.example.labelcallback;
+package pdbswitchingsample.callback;
 
 import java.io.IOException;
 import java.sql.Connection;
